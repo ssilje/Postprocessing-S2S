4089e09561611a772aeb13302652ac5bb1870761
Silje Sørland <silje.soerland@env.ethz.ch>
2020-12-16 15:51:53 +0100

    Update convert_grib2nc.sh

--- convert_grib2nc.sh
+++ convert_grib2nc.sh
@@ -19,13 +19,23 @@ day=$(echo ${date} | cut -d'-' -f3)
 
 HC='0'
         
-while [ ${HC} -le 20  ] ; do # 20 years hindcast
+#while [ ${HC} -le 20  ] ; do # 20 years hindcast
+while [ ${HC} -le 5  ] ; do # 20 years hindcast
 yHC=`expr ${y} - $HC`
 echo $yHC
 echo $day
 echo $date
 echo ${DATA_S2S}/tp_cf_${date}_hc_${yHC}-${m}-${day}.grb
+mkdir tmp
 cdo -f nc copy  ${DATA_S2S}/tp_cf_${date}_hc_${yHC}-${m}-${day}.grb ${workdir}/work.$$/tp_cf_${date}_hc_${yHC}-${m}-${day}.nc
+cdo splitday ${workdir}/work.$$/tp_cf_${date}_hc_${yHC}-${m}-${day}.nc ${workdir}/work.$$/tmp/tmp_
+
+cd ${workdir}/work.$$/tmp
+
+daynum=$(echo | ls -L | wc -l)
+
+echo $daynum
+
 HC=`expr ${HC} + 1`
 
 
