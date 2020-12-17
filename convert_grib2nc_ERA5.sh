6da4bb9ef61608f5f84922c5501af9e6b9b59cca
Silje Sørland <silje.soerland@env.ethz.ch>
2020-12-17 17:39:29 +0100

    Update convert_grib2nc_ERA5.sh

--- convert_grib2nc_ERA5.sh
+++ convert_grib2nc_ERA5.sh
@@ -23,6 +23,8 @@ m=$(echo ${date} | cut -d'-' -f2)
   cdo remapcon,r720x360  ${DATA_ERA5}/ERA5_${y}${m}.grb ${workdir}/ERA5_${y}${m}_r720x360.grb
   cdo sellonlatbox,-30,60,30,75  ${workdir}/ERA5_${y}${m}_r720x360.grb ${workdir}/ERA5_${y}${m}_r720x360_EUR.grb
   
-  cdo -f nc copy  ${workdir}/ERA5_${y}${m}_r720x360_EUR.grb ${workdir}/ERA5_${y}${m}_r720x360_EUR.nc 
+  cdo -f nc copy  ${workdir}/ERA5_${y}${m}_r720x360_EUR.grb ${workdir}/ERA5_${y}${m}_r720x360_EUR_unit-m.nc 
+  cdo mulc,1000 ERA5_${y}${m}_r720x360_EUR_unit-m.nc ERA5_${y}${m}_r720x360_EUR.nc # convert to kg/m2
+  ncatted -O -a units,pr,o,c,mm/day ERA5_${y}${m}_r720x360_EUR.nc
   rm ${workdir}/ERA5_${y}${m}_r720x360.grb ${workdir}/ERA5_${y}${m}_r720x360_EUR.grb 
   
