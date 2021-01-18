a1efb1c32de56cda8fd1403ec05eb9b78d0cbfb5
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-18 19:19:27 +0100

    Update PLot_SST_Bergen.py

--- PLot_SST_Bergen.py
+++ PLot_SST_Bergen.py
@@ -21,8 +21,33 @@ f = Dataset(ERAfile, mode='r')
 ERA_SST = f.variables['SST'][:]
 ERA_time = f.variables['time'][:]
 
+#time_var = nc.variables['time']
+dtime = netCDF4.num2date(ERA_time[:],ERA_time.units)
+
+first = netCDF4.num2date(ERA_time[0],ERA_time.units)
+last = netCDF4.num2date(ERA_time[-1],ERA_time.units)
+print first.strftime('%Y-%b-%d %H:%M')
+print last.strftime('%Y-%b-%d %H:%M')
+
+
 print(ERA_SST.shape)
 #print(TP[-1,:,:].shape)
 #print(TP[-1,:,:].min())
 #print(TP[-1,:,:].max())
 print(ERA_time)
+
+#data = {'apples': 10, 'oranges': 15, 'lemons': 5, 'limes': 20}
+#names = list(data.keys())
+#values = list(data.values())
+
+fig, ax = plt.subplots() # 
+#fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+#axs[0].bar(names, values)
+#axs[1].scatter(names, values)
+#axs[2].plot(names, values)
+ax.plot(time, dog, label="dog")
+ax.plot(activity, cat, label="cat")
+ax.legend()
+
+plt.show()
+fig.suptitle('Categorical Plotting')
