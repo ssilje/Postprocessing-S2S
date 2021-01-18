640b816eed5a76a9f25d12786dbc62d1cc90f7a4
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-18 19:22:00 +0100

    Update PLot_SST_Bergen.py

--- PLot_SST_Bergen.py
+++ PLot_SST_Bergen.py
@@ -7,7 +7,8 @@ Created on Mon Jan 18 18:43:49 2021
 """
 import numpy as np
 import xarray as xr
-from netCDF4 import Dataset
+import netCDF4
+#from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib as mpl
