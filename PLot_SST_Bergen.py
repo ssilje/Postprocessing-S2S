75b4dbcc6950383a92f41f356a039503ff8d6271
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-18 19:02:14 +0100

    Create PLot_SST_Bergen.py

---
+++ PLot_SST_Bergen.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 18:43:49 2021
+
+@author: siljesorland
+"""
+import numpy as np
+import xarray as xr
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib as mpl
+import cartopy as ccrs
+import cartopy.feature as cfeature
+import matplotlib.font_manager
+from mpl_toolkits.basemap import Basemap,shiftgrid
+
+ERA5file='ERA5_sst_1999-2019_07_Bergen_clim.nc'
+f = Dataset(ERAfile, mode='r')
+ERA_SST = f.variables['SST'][:]
+ERA_time = f.variables['time'][:]
+
+print(ERA_SST.shape)
+#print(TP[-1,:,:].shape)
+#print(TP[-1,:,:].min())
+#print(TP[-1,:,:].max())
+print(ERA_time)
