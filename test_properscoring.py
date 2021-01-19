92a511e35064383aa708de4108dd23260abf670d
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-19 19:19:17 +0100

    Create test_properscoring.py

---
+++ test_properscoring.py
@@ -0,0 +1,9 @@
+import numpy as np
+import properscoring as ps
+from scipy.stats import norm
+
+obs = [-2, -1, 0, 1, 2]
+baseline_score = ps.crps_ensemble(obs, [0, 0, 0, 0, 0]).mean()
+forecast_score = ps.crps_gaussian(obs, mu=0, sig=1).mean()
+skill = (baseline_score - forecast_score) / baseline_score
+print(skill)
