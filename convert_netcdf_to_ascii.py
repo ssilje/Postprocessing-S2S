417a4513e4b5b2152eff15629ed65b8ac490d52a
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-19 20:36:04 +0100

    Update convert_netcdf_to_ascii.py

--- convert_netcdf_to_ascii.py
+++ convert_netcdf_to_ascii.py
@@ -18,7 +18,7 @@ time = f.variables['time'][:]
 #print(f.shape)
 print(time.shape)
 print(sst.shape)
-time2= np.squeeze(time).shape
+time2= np.squeeze(time)
 print(time2)
 #data = xr.open_dataset("/home/python/PBLH_Exp_08_jul_2006.nc")
 #d = xr.DataArray(data.variables['PBLH'])
