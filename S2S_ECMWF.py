b79097816ed3f30a340c02cf62e45a19b2e8fd20
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-26 16:12:52 +0100

    Update S2S_ECMWF.py

--- S2S_ECMWF.py
+++ S2S_ECMWF.py
@@ -5,49 +5,13 @@ Created on Mon Jan 18 18:43:49 2021
 
 @author: siljesorland
 """
-import numpy as np
-import xarray as xr
-import netCDF4
-from netCDF4 import Dataset
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import matplotlib as mpl
-import cartopy as ccrs
-import cartopy.feature as cfeature
-import matplotlib.font_manager
-from mpl_toolkits.basemap import Basemap,shiftgrid
+import xarray as xr
+import cartopy.crs as ccrs
 
-ERAfile='ERA5_sst_1999-2019_07_Bergen_clim.nc'
-f = Dataset(ERAfile, mode='r')
-ERA_SST = f.variables['SST']
-ERA_time = f.variables['time']
+dirbase = '/nird/projects/NS9001K/sso102/S2S/netcdf'
+file='sst_CY46R1_2019-07-29_cf_EUR.nc'
+data = '%s/%s'%(dirbase,file)
 
-#time_var = nc.variables['time']
-dtime = netCDF4.num2date(ERA_time[:],ERA_time.units)
-first = netCDF4.num2date(ERA_time[0],ERA_time.units)
-last = netCDF4.num2date(ERA_time[-1],ERA_time.units)
-print(first.strftime('%Y-%b-%d %H:%M'))
-print(last.strftime('%Y-%b-%d %H:%M'))
-
-
-print(ERA_SST.shape)
-#print(TP[-1,:,:].shape)
-#print(TP[-1,:,:].min())
-#print(TP[-1,:,:].max())
-print(ERA_time)
-
-#data = {'apples': 10, 'oranges': 15, 'lemons': 5, 'limes': 20}
-#names = list(data.keys())
-#values = list(data.values())
-
-fig, ax = plt.subplots() # 
-#fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
-#axs[0].bar(names, values)
-#axs[1].scatter(names, values)
-#axs[2].plot(names, values)
-ax.plot(time, dog, label="dog")
-ax.plot(activity, cat, label="cat")
-ax.legend()
-
-plt.show()
-fig.suptitle('Categorical Plotting')
+#fN = 'data/ARGO_ATL_20171230.nc'
+#ATL = xr.open_dataset(fN)
