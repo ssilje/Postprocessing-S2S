197cfc9c9d49a0fc66a5b789590e610af646bcd6
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-27 19:32:29 +0100

    Update plot_SST.py

--- plot_SST.py
+++ plot_SST.py
@@ -18,11 +18,13 @@ dirbase = '/nird/projects/NS9001K/sso102/S2S/DATA/SST'
 fERA5='ERA5_sst_1999-2019_07.nc'
 ERA5 = '%s/%s'%(dirbase,fERA5)
 dataopen = xr.open_dataset(ERA5) 
-ERA5_BR = dataopen.sel(lat=lat, lon=lon, method='nearest').resample(time='Y').mean()
+ERA5_BR = dataopen.sel(lat=lat, lon=lon, method='nearest').resample(time='D').mean()
+#s = pd.Series(index=pd.date_range(start=dt.datetime(2014,1,1), end = dt.datetime(2015,6,1)), data = np.random.randn(517))
+
 print("ERA5 DS head 15")
 print(ERA5_BR.to_dataframe().head(15))
 #star_ERA5_BR=ERA5_BR.stat()
 fig = plt.figure(figsize=(15, 15))
-ERA5_BR.SST.plot()
-
+#ERA5_BR.SST.plot()
+ERA5_BR.groupby(ERA5_BR.index.month).mean().plot()
 fig.savefig('SST_Bergen_v2.png')
