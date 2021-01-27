bd9283c4c33d6b693d6b95b67141efe08d82ac0c
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-27 19:03:58 +0100

    Update plot_SST.py

--- plot_SST.py
+++ plot_SST.py
@@ -18,10 +18,10 @@ dirbase = '/nird/projects/NS9001K/sso102/S2S/DATA/SST'
 fERA5='ERA5_sst_1999-2019_07.nc'
 ERA5 = '%s/%s'%(dirbase,fERA5)
 dataopen = xr.open_dataset(ERA5) 
-ERA5_BR = dataopen.sel(lat=lat, lon=lon, method='nearest').resample(time='D').mean()
+ERA5_BR = dataopen.sel(lat=lat, lon=lon, method='nearest').resample(time='Y').mean()
 print("ERA5 DS head 15")
 print(ERA5_BR.to_dataframe().head(15))
-
+star_ERA5_BR=ERA5_BR.stat()
 fig = plt.figure(figsize=(15, 15))
 ERA5_BR.SST.plot()
 
