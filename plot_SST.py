54696188333a673393bfd141c380341dfb8e39de
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-27 20:35:52 +0100

    Update plot_SST.py

--- plot_SST.py
+++ plot_SST.py
@@ -19,14 +19,9 @@ fERA5='ERA5_sst_1999-2019_07.nc'
 ERA5 = '%s/%s'%(dirbase,fERA5)
 dataopen = xr.open_dataset(ERA5) 
 ERA5_BR = dataopen.sel(lat=lat, lon=lon, method='nearest').resample(time='D').mean()
-#s = pd.Series(index=pd.date_range(start=dt.datetime(2014,1,1), end = dt.datetime(2015,6,1)), data = np.random.randn(517))
-ERA5_BR_df=ERA5_BR.to_dataframe()
-#ERA5_BR_mon = '/'.join([d.replace('%i'%refyear,'%i'%i) for i in range(refyear-20,refyear)])
-#ERA5_BR_mon = ERA5_BR.groupby(ERA5_BR_df.index.'%i').mean()
-#[d.replace('%i'%refyear,'%i'%i) for i in range(refyear-20,refyear)])
 
-#for i in np.arange(1,32,1):
-#    ERA5_BR.groupby(ERA5_BR_df.index.i).mean()
+ERA5_BR_df=ERA5_BR.to_dataframe()
+ERA5_BR_df_mean=ERA5_BR_df.sel(day=slice(1,31)).mean()
     
 
 
@@ -35,5 +30,5 @@ print(ERA5_BR.to_dataframe().head(15))
 #star_ERA5_BR=ERA5_BR.stat()
 fig = plt.figure(figsize=(15, 15))
 #ERA5_BR.SST.plot()
-ERA5_BR.groupby(ERA5_BR_df.index.1).mean().plot()
+#ERA5_BR.groupby(ERA5_BR_df.index.1).mean().plot()
 fig.savefig('SST_Bergen_v2.png')
