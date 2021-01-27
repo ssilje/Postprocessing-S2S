2e81b7f14d12026d5f4f91e2bdc706df828510e2
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-27 20:12:24 +0100

    Update plot_SST.py

--- plot_SST.py
+++ plot_SST.py
@@ -21,6 +21,15 @@ dataopen = xr.open_dataset(ERA5)
 ERA5_BR = dataopen.sel(lat=lat, lon=lon, method='nearest').resample(time='D').mean()
 #s = pd.Series(index=pd.date_range(start=dt.datetime(2014,1,1), end = dt.datetime(2015,6,1)), data = np.random.randn(517))
 ERA5_BR_df=ERA5_BR.to_dataframe()
+#ERA5_BR_mon = '/'.join([d.replace('%i'%refyear,'%i'%i) for i in range(refyear-20,refyear)])
+#ERA5_BR_mon = ERA5_BR.groupby(ERA5_BR_df.index.'%i').mean()
+#[d.replace('%i'%refyear,'%i'%i) for i in range(refyear-20,refyear)])
+
+#for i in np.arange(1,32,1):
+#    ERA5_BR.groupby(ERA5_BR_df.index.i).mean()
+    
+
+
 print("ERA5 DS head 15")
 print(ERA5_BR.to_dataframe().head(15))
 #star_ERA5_BR=ERA5_BR.stat()
