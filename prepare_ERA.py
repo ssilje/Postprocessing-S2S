8c41b7a7ddcada7a0fd584855af927003908c614
Silje Sørland <silje.soerland@env.ethz.ch>
2021-02-02 13:35:34 +0100

    Update prepare_ERA.py

--- prepare_ERA.py
+++ prepare_ERA.py
@@ -11,18 +11,21 @@ var_long='sea_surface_temperature'
 
 # 10m_u_component_of_wind 2m_temperature sea_surface_temperature total_precipitation 10m_v_component_of_wind mean_sea_level_pressure snowfall
 
-DATE = pd.date_range(start='1999-07-01', end='2020-07-01')
+#DATE = pd.date_range(start='1999-07-01', end='2020-07-01')
+DATE = pd.date_range(start='1999-07-01', end='2000-07-01')
 
 DATAERA = '%s/%s/'%(DATABASE,'SFE/ERA_daily_nc')
 
-
+if not os.path.exists(workdir)  :
+                os.makedirs(workdir)
+        
 ## Regrid ERA
 for idate in DATE:
     d = idate.strftime('%Y%m%d')
     filein = '%s/%s_%s%s'%(DATAERA,var_long,d,'.nc')
     fileout_tmp = '%s/%s_%s%s'%(workdir,var_long,d,'_remapcon.nc')
-    fileout_EUR = '%s/%s_%s%s'%(workdir,var_long,d,'_remapcon_EUR.nc')
+    fileout_EUR = '%s/%s_%s%s'%(workdir,var_long,d,'_EUR1deg.nc')
     os.system('cdo remapcon,r360x181 ' + filein  + ' ' + fileout_tmp)
-    os.system('cdo remapcon,r360x181 ' + fileout_tmp  + ' ' + fileout_EUR)
+    os.system('cdo sellonlatbox,-30,60,30,75 ' + fileout_tmp  + ' ' + fileout_EUR)
     os.system('rm ' + fileout_tmp)
 
