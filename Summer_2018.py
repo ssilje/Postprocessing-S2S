34b6b7fec85652f79f557f5353cc1d113f907568
Silje Sørland <silje.soerland@env.ethz.ch>
2021-02-15 16:34:59 +0100

    Update Summer_2018.py

--- Summer_2018.py
+++ Summer_2018.py
@@ -16,6 +16,9 @@ var_short = 't2m'
 ftype = 'pf'
 product = 'hindcast' # forecast
 cycle = 'CY43R3_CY45R1'
+# Bergen
+lat = 60.23
+lon = 5.19
 
 #ds_grib = xr.open_dataset('data/weather/min_max_t_and_wind_gust.grib',engine='cfgrib')
 
@@ -39,8 +42,8 @@ for idate in dates_thursday:
         dS2S = '%s/%s/%s_%s_%s_%s%s'%(dir,var_short,var_short,cycle,d,ftype,'.grb')
         dataopen = xr.open_dataset(dS2S,engine='cfgrib')
         
-       # S2S_BR_daily = dataopen.sel(latitude=lat, longitude=lon, method='nearest').to_dataframe()
-        S2S_BR_daily = dataopen.to_dataframe()
+        S2S_BR_daily = dataopen.sel(latitude=lat, longitude=lon, method='nearest').to_dataframe()
+       # S2S_BR_daily = dataopen.to_dataframe()
         print(S2S_BR_daily.head(20))
         ## Loop through the whole forecast
         #forecast_leadtime = pd.date_range(dh, periods=46, freq="D")
